ec169c3d95c5a4547b93149eeadf0656b7eb21e5
abollepa <abollepa@charlotte.edu>
2025-05-03 22:48:04 -0400

    Add files via upload
    Tried to add the login system but it doesn't work

---
+++ Fit_Group/base/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/',views.loginPage,name="login"),
+    path('registration/',views.registrationPage,name='registration'),
+    path('',views.home,name="home"),
+    path('calendar/',views.calendar,name="calendar"),
+    path("settings/",views.settings,name="settings"),
+    path("groups/",views.groups,name="groups"),
+    path("leaderboard/",views.leaderboard,name="leaderboard"),
+    path('training_options/',views.training_options,name="training_room")
+]
---
+++ Fit_Group/base/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+# Create your views here.
+
+""" 
+Login system from: https://www.geeksforgeeks.org/user-authentication-system-using-django/
+"""
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if not User.objects.filter(username=username).exists():
+            messages.error(request,'Invalid Username')
+            return redirect('/login/')
+
+        student = authenticate(username=username,password=password)
+    
+        if student is None:
+
+            messages.error(request,'Invalid Username')
+            return redirect('/login/')
+        else:
+
+            login(request,student)
+            return redirect('/home/')
+    
+    return render(request,'login.html')
+
+def registrationPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        student = User.objects.Filter(username=username)
+
+        if student.exists():
+            messages.info(request, "Username already taken!")
+            return redirect("/register/")
+        
+        student = User.objects.create_user(
+            username=username
+        )
+
+        student.set_password(password)
+        student.save()
+
+        messages.info(request,"Welcome future olympian!")
+        return redirect('/register/')
+    
+    return render(request,'register.html')
+
+def home(request): # for looking at progress
+    return render(request, 'home.html')
+
+def calendar(request):
+    return render(request,'calendar.html')
+
+def settings(request):
+    return render(request, 'settings.html')
+
+def groups(request):
+    return render(request,'groups.html')
+
+def leaderboard(request):
+    return render(request,'leaderboard.html')
+
+def training_options(request):
+    return render(request, 'training_options.html')
